cbd606acc9bc3e671d0055e302940fd4569e5ad5
CurLeeBrace <leeandreimain@gmail.com>
2025-07-02 18:13:52 +0800

    modified base api url

--- client/src/components/Admin/Accounts.js
+++ client/src/components/Admin/Accounts.js
@@ -1,5 +1,3 @@
-import React from 'react';
-
 
 const table = () => {
   let thClass = 'border-2 border-amber-900 p-2'
--- client/src/customHooks/configAxios.js
+++ client/src/customHooks/configAxios.js
@@ -1,13 +1,12 @@
 import axios from "axios";
 
-// let ipAddress = "http://192.168.0.151:3001/api"
-// const localIP = 
+const BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:3001/api';
+
+
 export const api = axios.create({
-      baseURL: 'http://localhost:3001/api',
+      baseURL: BASE_URL,
 
 
-      
-      
 })
 
 
